6732d9b1a9c0d5ea95ed9694167d6c3b2e19e231
zj831007 <zj831007@163.com>
2013-04-29 21:20:44 +0800

    Initial Commit

---
+++ jsbtest/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  jsbtestAppController.h
+//  jsbtest
+//
+//  Created by Justin on 13-4-29.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ jsbtest/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  jsbtestAppController.h
+//  jsbtest
+//
+//  Created by Justin on 13-4-29.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
